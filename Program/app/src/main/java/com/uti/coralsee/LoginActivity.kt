560dff0eb9e2479f73b80ecc76a2cc476559f21b
cecep1407 <cecep1407@gmail.com>
2024-06-03 12:41:57 +0700

    Master : menambah activity dan fragment login

---
+++ Program/app/src/main/java/com/uti/coralsee/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.uti.coralsee
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.uti.coralsee.fragments.InputLoginFragment
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        supportFragmentManager.beginTransaction().add(R.id.frm_containter_login, InputLoginFragment()).commit()
+    }
+}
